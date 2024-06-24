76aa9235598edb0c8c3714ef57ef55e15b0ce54d
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-06-24 17:46:19 +0000

    validation using zod library

---
+++ zod.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const zod = require("zod");
+const app = express();
+
+const schema = zod.array(zod.number());
+
+// {
+//     email: string=> email
+//     password : atleast 8 letters
+//     country: "IN", "US"
+// }
+
+const schemaa = zod.object({
+    email : zod.string().email(),
+    password: zod.string().min(8),
+    country: zod.literal("IN").or(zod.literal("US"))
+})
+app.use(express.json());
+
+app.post("/health-checkup", function(req, res){
+
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+    if(!response.success){
+        res.status(411).json({
+            msg : "Input is invalid"
+        })
+    }
+    else{    
+        res.send({
+        response
+    })}
+});
+
+app.listen(3000);
