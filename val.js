1e825a81ad7da567d0084813eb2d29aa88b5e736
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-07-01 23:59:06 +0530

    Update val.js

--- val.js
+++ val.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.post("/health-checkup", function(req, res){
 
     const kidneys = req.body.kidneys;
-    const kidneyLength = kidneys.length;
+    const kidneyLength = kidneys.length; 
 
     res.send("you have "+ kidneyLength + " kidneys");
 });
